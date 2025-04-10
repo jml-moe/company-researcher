7daf98ab63d4e934d10817a1740ca962374d0264
Indra Zulfi <me@indrazm.com>
2025-04-11 00:18:31 +0700

    done

---
+++ researcher/utils.py
@@ -0,0 +1,7 @@
+from tavily import TavilyClient
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+tavily_client = TavilyClient(api_key=TAVILY_API_KEY)
